535a26a612f1e1156855f13899594f257aa01fd8
Shyamsundar Barman <shyamsundarbarman373@gmail.com>
2025-01-08 20:51:44 +0530

    Day one

---
+++ Day one/Arithmatic.java
@@ -0,0 +1,48 @@
+/*Write a Java program to define a class Arithmetic with two member variable and following methods. 
+#1 add() to do sum of to numbers and 
+#2 sub() to do subtraction of two numbers
+#3 mult() to multiply 2 numbers and 
+#4 division two divide  two numbers.
+and print the result*/
+
+class Arithmatic{
+  int x,y;
+void getdata(int a, int b){
+x=a;
+y=b;
+}
+void add(){
+ System.out.println("The sum of "+x+" and "+y+" is "+(x+y));
+}
+
+void sub(){
+System.out.println("The difference of "+x+" and "+y+" is "+(x-y));
+
+}
+
+void mult(){
+System.out.println("The multiplication of "+x+" and "+y+" is "+(x*y));
+
+}
+
+void div(){
+System.out.println("The division of "+x+" and "+y+" is "+(x/y));
+
+}
+
+public static void main(String args[]){
+Arithmatic obj1=new Arithmatic();
+Arithmatic obj2=new Arithmatic();
+obj1.getdata(10,6);
+obj1.add();
+obj1.sub();
+obj1.mult();
+obj1.div();
+obj2.getdata(21,11);
+obj2.add();
+obj2.sub();
+obj2.mult();
+obj2.div();
+
+}
+}
---
+++ Day one/New.java
@@ -0,0 +1,11 @@
+/*Write a Java program to take input using command line argument, "food" 3 favorite food of yourself. 
+"Book" favorite books of yourself. 
+"Places"  favorite places of yourself.
+*/
+class New{
+
+public static void main(String args[]){
+System.out.println("Favourite "+args[0]+" are\n "+args[1]+" and\n "+args[2]+" and\n "+args[3]);
+} 
+}
+
