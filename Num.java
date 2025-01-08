7f29e25756b6a3af071ebc779594a0601f05f9bb
Shyamsundar Barman <shyamsundarbarman373@gmail.com>
2025-01-08 20:33:37 +0530

    .

---
+++ Num.java
@@ -0,0 +1,16 @@
+//Write a java program to define a class Num with a single variable and the following method get() method to initialize the variable,show() method to print the value of the variable ,main method to create object of the class Num and call the method get() and show().
+
+class Num{
+int x;
+void get(int y){
+x=y;
+}
+void show(){
+System.out.println(" The value of the number is: "+x);
+}
+public static void main(String args[]){
+First obj=new First();
+obj.get(35);
+obj.show();
+}
+}
