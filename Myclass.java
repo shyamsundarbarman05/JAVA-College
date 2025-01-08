21bf52b872aef1826deb158bc7fdf9fa74f7654f
Shyamsundar Barman <shyamsundarbarman373@gmail.com>
2025-01-08 20:24:57 +0530

    first commit

---
+++ Myclass.java
@@ -0,0 +1,9 @@
+
+// Write a java program using a class name Myclass that consist of a single method ,print your name :<your name>
+// Department:<your Department> using that method.
+
+class Myclass{
+public static void main(String args[]){
+System.out.println("Name: Shyamsundar Barman"+"\nDepartment: CSE");
+}
+}
