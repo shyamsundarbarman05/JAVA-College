5f40a997c17501301b7d5eb8ba59a747f6457f59
Shyamsundar Barman <shyamsundarbarman373@gmail.com>
2025-01-14 20:04:15 +0530

    .

--- Day one/Arithmatic.java
+++ Day one/Arithmatic.java
@@ -2,13 +2,13 @@
 #1 add() to do sum of to numbers and 
 #2 sub() to do subtraction of two numbers
 #3 mult() to multiply 2 numbers and 
-#4 division two divide  two numbers.
+#4 div() two divide  two numbers.
 and print the result
 get() method to initialize two variable, create two objects of Arithmetic class and perform all operations*/
 
 class Arithmatic{
   int x,y;
-void getdata(int a, int b){
+void get(int a, int b){
 x=a;
 y=b;
 }
@@ -34,12 +34,12 @@ System.out.println("The division of "+x+" and "+y+" is "+(x/y));
 public static void main(String args[]){
 Arithmatic obj1=new Arithmatic();
 Arithmatic obj2=new Arithmatic();
-obj1.getdata(10,6);
+obj1.get(10,6);
 obj1.add();
 obj1.sub();
 obj1.mult();
 obj1.div();
-obj2.getdata(21,11);
+obj2.get(21,11);
 obj2.add();
 obj2.sub();
 obj2.mult();
